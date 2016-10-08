ca11d463063c74d96856cdd7d3177169b4ac1275
Shadowsun <wendyfreedom31@hotmail.com>
2016-10-08 17:49:56 +0200

    je fais de la merde avec mes menus

---
+++ Assets/Dev/S_ButtonManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class S_ButtonManager : MonoBehaviour
+{
+
+    public GameObject m_MainMenu, m_SelectPlayer, m_Credits;
+
+    public Button m_StartGameButton;
+
+    #region MainMenu
+
+    public void Menu_Start()
+    {
+        m_SelectPlayer.SetActive( true );
+        m_MainMenu.SetActive( false );
+    }
+
+    public void Menu_Credits()
+    {
+
+        m_Credits.SetActive( true );
+        m_MainMenu.SetActive( false );
+    }
+
+    public void Menu_Quit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+  
+
+    void Update()
+    {
+        if( m_SelectPlayer.activeInHierarchy )
+        {
+            if( Input.GetButtonDown( "Cancel" ) )
+            {
+                m_SelectPlayer.SetActive( false );
+                m_MainMenu.SetActive( true );
+                m_StartGameButton.Select();
+
+            }
+
+            if( Input.GetButtonDown( "Submit" ) )
+            {
+                // Add a player
+            }
+        }
+
+        if( m_Credits.activeInHierarchy )
+        {
+            if( Input.GetButtonDown( "Cancel" ) )
+            {
+                m_Credits.SetActive( false );
+                m_MainMenu.SetActive( true );
+                m_StartGameButton.Select();
+                           
+            }
+        }
+      
+        
+
+    }
+
+
+}
