2f327c25b84dd1d4364df560cbb21a8db242ebc7
Shadowsun <wendyfreedom31@hotmail.com>
2016-10-08 21:10:49 +0200

    je vais dormir

---
+++ Assets/Dev/S_GameOverManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class S_GameOverManager : MonoBehaviour
+{
+
+    public void Button_Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Button_MainMenu()
+    {
+        SceneLoader.singleton.changeScene( "MenuScene" );
+    }
+
+    public void Button_Restart()
+    {
+        SceneLoader.singleton.changeScene( "MainScene" );
+    }
+
+}
