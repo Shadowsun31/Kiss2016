42c1f7db08bf5ab055e96ad9ba0153a185fc770b
Baptiste Remy <alibatiste@gmail.com>
2016-10-08 19:37:23 +0200

    fuck my message

--- Assets/MigrantPlayer.cs
+++ Assets/MigrantPlayer.cs
@@ -6,15 +6,41 @@ public class MigrantPlayer : Migrant {
     [SerializeField]
     private int playerNum;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    [SerializeField]
+    private float jump;
+
+    [SerializeField]
+    private float deplacement;
+
+
+    [SerializeField]
+    private new Rigidbody2D rigidbody2D;
+
+    private bool isJumping = false;
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+        if (Input.GetButton("Joy"+(playerNum+1) + "_ButA"))
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                StartCoroutine(jumpIenum());
+            }
+        }
+
+        rigidbody2D.AddForce(new Vector2(Input.GetAxisRaw("Joy" + (playerNum + 1) + "_Horizontal"), 0), ForceMode2D.Force);
+
+    }
+
+    public IEnumerator jumpIenum()
+    {
+        rigidbody2D.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1);
+        isJumping = false;
+
+    }
 
     public void SetNumPlayer(int playerNum)
     {
