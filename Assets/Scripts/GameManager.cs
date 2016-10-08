280359d84d285a798ecb5834652382eb86d00bc6
Baptiste Remy <alibatiste@gmail.com>
2016-10-08 11:26:31 +0200

    J'attaque les scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager singleton;
+
+    public void Awake()
+    {
+        singleton = this;
+    }
+
+    public void playerDie(int numPlayer)
+    {
+        GameData.singleton.playerAlive[numPlayer] = false;
+
+        bool StillPlayerAlive = false;
+        for(int i = 0;i < GameData.singleton.PlayerMax; i++)
+        {
+            if(GameData.singleton.playerConnected[i] && GameData.singleton.playerAlive[i])
+            {
+                StillPlayerAlive = true;
+            }
+        }
+        if (!StillPlayerAlive)
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Events;
+
+public class SceneLoader : MonoBehaviour {
+
+    [SerializeField]
+    private RawImage fadeImage;
+
+    [SerializeField]
+    private float duration = 0.5f;
+
+    public static SceneLoader singleton;
+
+    public UnityEvent onFinishFade;
+
+    public void Awake()
+    {
+        singleton = this;
+        fadeImage.color = Color.black;
+        StartCoroutine(IntroFade());
+    }
+
+    public void changeScene(string nextScene)
+    {
+        StartCoroutine(changeSceneFade(nextScene));
+    }
+
+    IEnumerator changeSceneFade(string nextScene)
+    {
+        float startTime = Time.time;
+        float normTime = 0;
+
+        while(normTime < 1)
+        {
+            normTime = (Time.time - startTime) / duration;
+            fadeImage.color = Color.Lerp(Color.clear, Color.black, normTime);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+    }
+
+    IEnumerator IntroFade()
+    {
+        float startTime = Time.time;
+        float normTime = 0;
+
+        while (normTime < 1)
+        {
+            normTime = (Time.time - startTime) / duration;
+            fadeImage.color = Color.Lerp(Color.grey, Color.clear, normTime);
+            yield return null;
+        }
+        onFinishFade.Invoke();
+    }
+
+
+
+}
