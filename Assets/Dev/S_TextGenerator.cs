ba8fc0d9b7c4c8a1bd9e680c357bcf263a2a3d37
Shadowsun <wendyfreedom31@hotmail.com>
2016-10-09 13:39:00 +0200

    menu & gameover

---
+++ Assets/Dev/S_TextGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_TextGenerator : MonoBehaviour {
+
+    private string[] listName = {
+
+    };
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
