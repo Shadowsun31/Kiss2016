0bf486e036760d2888db538b41082f380410b237
Baptiste Remy <alibatiste@gmail.com>
2016-10-09 14:41:09 +0200

    j

---
+++ Assets/popLandau.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popLandau : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (Random.Range(0, 2) == 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
